fdfbc740a8b32df7bf8e8216a23c3474ff47e9df
kespinal83 <kespinal@gradcenter.cuny.edu>
2020-09-21 12:49:46 -0400

    Draft Load for Test

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -1,74 +1,139 @@
 /* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * 0.7,
   height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 },
+  margin = { top: 20, bottom: 50, left: 40, right: 40 },
   radius = 5;
 
-// these variables allow us to access anything we manipulate in init() but need access to in draw().
-// All these variables are empty before we assign something to them.
 let svg;
 let xScale;
 let yScale;
 
-/* APPLICATION STATE */
 let state = {
   data: [],
-  selection: "All" // + YOUR FILTER SELECTION
+  selection: "All"
 };
 
-/* LOAD DATA */
-d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
-  // + SET YOUR DATA PATH
+d3.json("../data/data.json", d3.autoType).then(raw_data => {
   console.log("raw_data", raw_data);
   state.data = raw_data;
+  console.log("state", state)
   init();
 });
 
-/* INITIALIZING FUNCTION */
-// this will be run *one time* when the data finishes loading in 
 function init() {
-  // + SCALES
 
-  // + AXES
+const selectElement = d3.select("#dropdown").on("change", function() {
+  state.selection = this.value
+  console.log("new value is", this.value);
+  console.log(state)
+  draw();
+});
 
-  // + UI ELEMENT SETUP
-
-  const selectElement = d3.select("#dropdown").on("change", function() {
-    // `this` === the selectElement
-    // 'this.value' holds the dropdown value a user just selected
-
-    state.selection = this.value
-    console.log("new value is", this.value);
-    draw(); // re-draw the graph based on this new selection
-  });
-
-  // add in dropdown options from the unique values in the data
-  selectElement
-    .selectAll("option")
-    .data(["All", "1", "2", "3"]) // + ADD UNIQUE VALUES
-    .join("option")
+selectElement
+  .selectAll("option")
+    .data(["All", "September", "August", "July"])  //Wow make sure your data has no hidden spaces or padding
+  .join("option")
     .attr("value", d => d)
     .text(d => d);
 
-  // + CREATE SVG ELEMENT
+svg = d3.select("#d3-container")
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height)
 
-  // + CALL AXES
+xScale = d3.scaleLinear()
+  .domain([0, 1000])
+  .range([margin.left, width - margin.right]);
 
-  draw(); // calls the draw function
+yScale = d3.scaleLinear()
+  .domain([0, 160])
+  .range([height - margin.bottom, margin.top]);
+
+const xAxis = d3.axisBottom(xScale);
+const yAxis = d3.axisLeft(yScale);
+
+svg // add the xAxis
+  .append("g")
+  .attr("class", "myX-axis")
+  .attr("transform", `translate(0,${height - margin.bottom})`)
+  .call(xAxis)
+  .append("text")
+  .attr("class", "axis-label")
+  .attr("x", "50%")
+  .attr("dy", "3em")
+  .text("Turnaround Time Metrics (Collection to Completed (minutes))");
+
+svg //add the yAxis
+  .append("g")
+  .attr("class", "myY-axis")
+  .attr("transform", `translate(${margin.left},0)`)
+  .call(yAxis)
+  .append("text")
+  .attr("class", "axis-label")
+  .attr("y", "50%")
+  .attr("dx", "-3em")
+  .attr("writing-mode", "vertical-rl")
+  .text("Result Value");
+  draw();
 }
-
-/* DRAW FUNCTION */
- // we call this everytime there is an update to the data/state
 function draw() {
-  
-  // + FILTER DATA BASED ON STATE
 
-  // const dot = svg
-  //   .selectAll("circle")
-  //   .data(filteredData, d => d.name)
-  //   .join(
-  //     enter => enter, // + HANDLE ENTER SELECTION
-  //     update => update, // + HANDLE UPDATE SELECTION
-  //     exit => exit // + HANDLE EXIT SELECTION
-  //   );
+let filteredData = state.data
+  if (state.selection !== "All") {
+    filteredData = state.data.filter(d => d.monthOrder === state.selection)
+  }
+
+console.log(filteredData)
+
+const dot = svg.selectAll(".dot")
+    .data(filteredData, d => d.specimenID)
+    .join(
+      enter => enter
+      .append("circle")
+      .attr("class", "dot")
+      .attr("cx", d => xScale(d["tatMin"]))
+      .attr("cy", d => yScale(d["result"]))
+      .attr("r", 7)
+      .style("opacity", 0.7)
+      .style("stroke", "white")
+      .attr("fill", d => {
+        if (d.tatMin >= "240") return "#4B584F";
+        else if (d.tatMin >= "20" && d.tatMin <= "60") return "#788D66";
+        else return "#DCD19c";
+      })
+      .attr("r", radius)
+      .attr("cy", d => yScale(d.result))
+      .attr("cx", d => margin.left) // initial value - to be transitioned
+      .call(enter =>
+        enter
+        .transition()
+        .delay(function(d,i){return(i*3)})
+        .duration(5000)
+        .attr("cx", d => xScale(d.tatMin))
+//      .attr("cx", function (d) { return x(d.tatMin); } ) 
+//      .attr("cy", function (d) { return y(d.result); } )
+      ),
+    
+      update => update.call(update =>
+        // update selections -- all data elements that match with a `.dot` element
+        update
+          .transition()
+          .duration(250)
+          .attr("stroke", "black")
+          .transition()
+          .duration(250)
+          .attr("stroke", "lightgrey")
+      ),
+      exit =>
+        exit.call(exit =>
+          // exit selections -- all the `.dot` element that no longer match to HTML elements
+          exit
+            .transition()
+            .delay(d => 50 * d.result)
+            .duration(500)
+            .attr("cx", width)
+            .remove()
+      )
+  );
+   
 }
