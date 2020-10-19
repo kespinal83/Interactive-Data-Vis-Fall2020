f19ace4c55b5f6f49dba1c5e25b64d1cf2292513
kespinal83 <kespinal@gradcenter.cuny.edu>
2020-10-18 21:06:06 -0400

    Final Draft

--- tutorial5_geographic/main.js
+++ tutorial5_geographic/main.js
@@ -1,58 +1,126 @@
-/**
- * CONSTANTS AND GLOBALS
- * */
 const width = window.innerWidth * 0.9,
   height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 };
+  margin = { top: 20, bottom: 50, left: 60, right: 40 }
 
-/** these variables allow us to access anything we manipulate in
- * init() but need access to in draw().
- * All these variables are empty before we assign something to them.*/
 let svg;
 
-/**
- * APPLICATION STATE
- * */
 let state = {
-  // + SET UP STATE
+  geojson: null,
+  shootings: null,
+  hover: {
+    latitude: null,
+    longitude: null,
+    state: null,
+  },
 };
 
-/**
- * LOAD DATA
- * Using a Promise.all([]), we can load more than one dataset at a time
- * */
 Promise.all([
-  d3.json("PATH_TO_YOUR_GEOJSON"),
-  d3.csv("PATH_TO_ANOTHER_DATASET", d3.autoType),
-]).then(([geojson, otherData]) => {
-  // + SET STATE WITH DATA
-  console.log("state: ", state);
+  d3.json("../data/usState.geojson"),
+  d3.json("../data/fatalPS.geojson"),
+]).then(([geojson, shootings]) => {
+  state.geojson = geojson;
+  state.shootings = shootings;
+  console.log(state)
   init();
 });
 
-/**
- * INITIALIZING FUNCTION
- * this will be run *one time* when the data finishes loading in
- * */
 function init() {
-  // create an svg element in our main `d3-container` element
-  svg = d3
-    .select("#d3-container")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+const projection = d3.geoAlbersUsa().fitSize([width, height], state.geojson);
+//const projection = d3.geoAlbersUsa().scale(1300).translate([487.5, 305])
+const path = d3.geoPath().projection(projection);
 
-  // + SET UP PROJECTION
-  // + SET UP GEOPATH
+svg = d3
+  .select("#d3-container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .call(d3.zoom().on("zoom", function () {
+  svg.attr("transform", d3.event.transform)
+}));
 
-  // + DRAW BASE MAP PATH
-  // + ADD EVENT LISTENERS (if you want)
+//svg = d3  //Exploring with Canvas - only works if no updates are needed
+//   .select("#d3-container")  //Exploring with Canvas
+   
+//const chart = svg //Exploring with Canvas
+//   .append("canvas") //Exploring with Canvas
+//   .attr("width", width) //Exploring with Canvas
+//   .attr("height", height) //Exploring with Canvas
+//   .call(d3.zoom().on("zoom", function () { //Exploring with Canvas
+//     svg.attr("transform", d3.event.transform) //Exploring with Canvas
+//  })); //Exploring with Canvas
 
-  draw(); // calls the draw function
-}
+//    var context = chart.node().getContext("2d"); //Exploring with Canvas
 
-/**
- * DRAW FUNCTION
- * we call this everytime there is an update to the data/state
- * */
-function draw() {}
+// Fonts
+
+    svg //draw the USofA
+    .selectAll(".state")
+    .data(state.geojson.features)
+    .join("path")
+    .attr("d", path)
+    .attr("class", "state")
+    .attr("fill", "transparent")
+    .on("mouseover", d => {
+      state.hover["state"] = d.properties.NAME; // when the mouse rolls over this feature, display coordinates
+      draw(); 
+    });
+
+    svg
+    .selectAll("circle")
+    .data(state.shootings.features)
+    .join(
+      enter => enter
+        .append("circle")
+        .attr("fill", "blue")
+        .style("opacity",0.2)
+        .style("stroke", "white")
+        .attr("r", 3)
+        .transition()
+          .attr("fill", "red")
+          .style("opacity",0.2)
+          .delay(function(d,i){return(i*2)})
+          .duration(2000)
+          .attr("cx",function(d) { return projection(d.geometry.coordinates)[0]})
+          .attr("cy",function(d) { return projection(d.geometry.coordinates)[1]})
+        .call(enter => enter),
+          update => update.call(update => update),
+          exit => exit.call(exit => exit
+          .remove()
+        
+    .on("mouseover",function(d) {
+    	  console.log("just had a mouseover", d3.select(d));
+        d3.select(this)
+          .classed("active",true)
+        })
+
+  	.on("mouseout",function(d){
+        d3.select(this)      	
+        .classed("active",false)
+        }),
+
+    svg.on("mousemove", () => {
+        const [mx, my] = d3.mouse(svg.node());
+        const proj = projection.invert([mx, my]);
+        state.hover["longitude"] = proj[0];
+        state.hover["latitude"] = proj[1];
+      draw();
+    })),)
+  
+    draw();
+  }
+  
+  function draw() {
+    hoverData = Object.entries(state.hover);
+  
+    d3.select("#hover-content")
+      .selectAll("div.row")
+      .data(hoverData)
+      .join("div")
+      .attr("class", "row")
+      .html(
+        d =>
+          d[1]
+            ? `${d[0]}: ${d[1]}`
+            : null
+      );
+  }
